f6999b518e8505bebd5903330de18afd7dee3012
Abhishek Vaish <abhishek953566936222@gmail.com>
2022-06-04 15:17:53 +0530

    new ui

--- js/main.js
+++ js/main.js
@@ -1,158 +1,24 @@
-document.addEventListener('DOMContentLoaded', function() {
-	console.log("loaded....")
+const heroElement = document.querySelector(".hero h1")
+heroElement.herotext = "Hi I am Abhishek \nनमस्ते मैं अभिषेक हूँ \nनमस्कार मी अभिषेक \nHi I am Abhishek"
 
-	var elems = document.querySelectorAll('.sidenav');
-	var instances = M.Sidenav.init(elems,{});
-	var skimgs = document.querySelectorAll('#skills .skimg')
-	skimgs[0].style.transform = "translate(-200%,-100%)"
-	skimgs[1].style.transform = "translate(-550%,-200%)"
-	skimgs[2].style.transform = "translate(-100%,-250%)"
-	skimgs[3].style.transform = "translate(10%,-200%)"
-	skimgs[4].style.transform = "translate(180%,-150%)"
-	skimgs[5].style.transform = "translate(300%,-30%)"
-	skimgs[6].style.transform = "translate(-150%,50%)"
-	skimgs[7].style.transform = "translate(-170%,200%)"
-	skimgs[8].style.transform = "translate(100%,200%)"
-	skimgs[9].style.transform = "translate(-40%,250%)"
-	skimgs[10].style.transform = "translate(340%,60%)"
-	skimgs[11].style.transform = "translate(0%,70%)"
-	skimgs[12].style.transform = "translate(-140%,-150%)"
-});
-gsap.registerPlugin(ScrollTrigger)
-gsap.registerPlugin(ScrollToPlugin);
+function animateHeroText(index) {
+	if (index >= heroElement.herotext.length) {
+		heroElement.parentElement.children[1].style.visibility = "hidden" // .remove()
+		return
+	}
 
+	if (heroElement.herotext[index] == "\n") {
+		heroElement.innerText = heroElement.innerText
+		if (heroElement.innerText.length > 0) {
+			heroElement.innerText = heroElement.innerText.slice(0, heroElement.innerText.length - 1)
+			return setTimeout(() => animateHeroText(index), 100)
+		}
+		return setTimeout(() => animateHeroText(index + 1), 100)
+	}
+	heroElement.innerText += heroElement.herotext[index]
+	setTimeout(() => animateHeroText(index + 1), 100)
+}
+animateHeroText(0)
 
-
-//---------------------------------- navbar --------------------------------------
-gsap.to("nav",{
-	scrollTrigger:{
-		trigger:"#project",
-		start : "top center",
-		toggleActions : "play none none reset"
-	},
-	duration : 1,
-	position:"fixed",
-	opacity:1,
-	zIndex:2
-
-})
-
-// navbar scrollTo Section
-
-// Scroll to the element in the URL's hash on load
-
-gsap.to(window, {scrollTo: window.location.hash ,duration: 1})
-
-document.querySelectorAll('nav ul a').forEach(a => {
-	hash = a.hash
-	// add the scroll to href option
-	a.addEventListener('click', e => {
-		e.preventDefault()
-		gsap.to(window, {scrollTo: a.hash ,duration: 1})
-	});
-	// add the scrollspy option
-	gsap.to(a.parentNode , {
-		scrollTrigger :{
-			trigger : a.hash,
-			onToggle: self => { a.parentNode.classList.toggle('active')},
-			start : "top center",
-			end : "bottom center"
-		},		
-	})
-	
-});
-
-
-//----------------------------------------------------- heading introduction text
-
-heading_timeline = gsap.timeline()
-heading_timeline
-.from(".heading img",{ duration: 1,y:-40,opacity :0 , ease : "bounce",delay:1})
-.to(".heading h1",{duration: 2,text: "I am Abhishek Vaish"})
-.to(".heading h1",{duration: 2,text: "A Developer"})
-.to(".heading h1",{duration: 2,text: "An Engineer"})
-.to(".heading h1",{duration: 2,text: "I am Abhishek Vaish"})
-gsap.from(".box", {
-	duration: 2,
-	scale: 0.5, 
-	opacity: 0, 
-	delay: 0.5, 
-	stagger: 0.2,
-	ease: "elastic", 
-	force3D: true
-});
-
-
-
-//-----------------------------------------------------------projects animations
-gsap.set("#project .card",{opacity:0,y:100})
-ScrollTrigger.batch("#project .card", {
-	interval: 0.1,
-	batchMax: 3, 
-	onEnter: batch => gsap.to(batch, {opacity: 1, y: 0, stagger: 0.2}),
-	onLeave: batch => gsap.to(batch, {opacity: 0, y: -100,stagger: 0.2}),
-	onEnterBack: batch => gsap.to(batch, {opacity: 1, y: 0, stagger: 0.2}),
-	onLeaveBack: batch => gsap.to(batch, {opacity: 0, y: 100}),
-	start: 'top center+=300',
-	end: 'bottom top',
-	// markers:true
-});
-// ScrollTrigger.addEventListener("refreshInit", () => gsap.set(".card", {y: 0,opacity:1}));
-
-
-
-
-
-//--------------------------------------------------------CONTACT
-gsap.from("#contact i.large",{
-	scrollTrigger:{
-		trigger:"#contact",
-		// markers : true,
-		start : 'top center+=200',
-		end : 'bottom top',
-	},
-	opacity :0,
-	y:100,
-	duration : .5,
-	ease :'power1.inOut'
-}) 
-
-
-// --------------------------------------------EXPERIENCE
-
-ScrollTrigger.create({
-	trigger : "#experience",
-	start : 'top center+=200',
-	end : "bottom top",
-	// markers : true,
-	onEnter : () => gsap.from('.experience .card-panel',{y:100,opacity :0 ,duration : 1 , ease: "power1.inOut" ,stagger : 0.4}),
-	onEnterBack : () => gsap.from('.experience .card-panel',{y:-100,opacity :0 ,duration : .5 , ease: "power1.inOut" ,stagger : { each: 0.4 , from : 'end'}}),
-
-})
-
-
-// ----------------------------------------------EDUCATION
-ScrollTrigger.create({
-	trigger : "#education",
-	start : 'top center+=200',
-	end : "bottom top",
-	// markers : true,
-	onEnter : () => gsap.from('.education .card-panel',{y:100,opacity :0 ,duration : .5 , ease: "power1.inOut" ,stagger : 0.4}),
-	onEnterBack : () => gsap.from('.education .card-panel',{y:-100,opacity :0 ,duration : .5 , ease: "power1.inOut" ,stagger : { each: 0.4 , from : 'end'}}),
-
-})
-
-//---------------------------------------------------SKILLS
-gsap.set('.skimg-container',{y:300})
-gsap.to('.skimg-container',{
-	scrollTrigger : {
-		trigger : '#skills',
-		scrub : 1,
-		// markers : true,
-		start :'center-=200 center+=200',
-		end : 'center center',
-
-	},
-	y : -100,
-	duration : 3
-})
+const navItems = document.querySelector(".nav-items")
+// document.getElementById("hamburger").onclick = () => {}
